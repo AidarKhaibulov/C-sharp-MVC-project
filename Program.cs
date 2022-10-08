ad4349c8d00c8e1456dd3ba013b3b697084ac834
AidarKhaibulov <myakkaynt228@gmail.com>
2022-10-08 12:50:34 +0300

    1

--- Program.cs
+++ Program.cs
@@ -3,13 +3,9 @@ using WebMVC;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// получаем строку подключения из файла конфигурации
 string connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
-// добавляем контекст ApplicationContext в качестве сервиса в приложение
 builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
